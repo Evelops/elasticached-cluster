34908c966ea6e021683a2e51cadf8e674b78c7a5
eomseung-yeol <co6747@naver.com>
2023-12-10 21:33:13 +0900

    chore: setup prettier & eslint

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 > Install Module
 
 ```bash
-npm i express compression cors morgan multer
+npm i express compression cors morgan multer dotenv
 npm i -D nodemon typescript ts-node @types/cors @types/express @types/multer @types/compression @types/morgan
-npm i dotenv
+npm install eslint prettier eslint-plugin-prettier eslint-config-prettier eslint-plugin-node husky --save-dev
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-import express, {Express, Application, Request, Response} from 'express';
-import multer, {Multer} from 'multer';
+import express, { Express, Application, Request, Response } from 'express';
+import multer, { Multer } from 'multer';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
-
 dotenv.config();
 
-const app: Application = express();
+const app: Express = express();
 
 app.set('port', process.env.PORT || 4000);
 
@@ -17,10 +16,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 //Router
-app.get('/',(req:Request, res: Response)=> {
-    res.send('Root Router');
-})
+app.get('/', (req: Request, res: Response) => {
+  res.send('Root Router');
+});
 
 export default app;
