ca6200671a907d1ccaf7731a6d5d9feb8867102b
eomseung-yeol <co6747@naver.com>
2023-12-10 19:36:09 +0900

    setup practice

--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 > Install Module
 
 ```bash
-npm i express typescript @types/express
-npm i -D nodemon ts-node
+npm i express @types/express
+npm i -D nodemon typescript ts-node @types/cors @types/multer 
 npm i dotenv
+npm i cors 
 ```
--- index.ts
+++ index.ts
@@ -1,4 +1,20 @@
-import express, {Express, Request, Response} from "express";
+import express, {Express, Application, Request, Response} from "express";
+import multer, {Multer} from "multer";
+import dotenv from "dotenv";
 
-const app: Express = express();
+dotenv.config();
+
+const app: Application = express();
+
+app.set("port", process.env.PORT || 4000);
+
+app.get("/",(req:Request, res: Response)=> {
+    res.send("Root Router");
+})
+
+app.listen(app.get("port"), ()=>{
+    console.log("start running server: ",app.get("port"));
+})
+
+
 
